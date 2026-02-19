12d9f41f441bd1ee37f3db1062bc72b88d7bfbc8
venky-21509 <venkeypothem21@gmail.com>
2026-02-19 10:41:30 +0000

    Remove obsolete sidekiq initializer

--- config/initializers/sidekiq.rb
+++
@@ -1,2 +0,0 @@
-Sidekiq.configure_server { |config| config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379/0' } }
-Sidekiq.configure_client { |config| config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379/0' } }
